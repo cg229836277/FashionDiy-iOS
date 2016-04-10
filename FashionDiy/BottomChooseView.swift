ed5d6140c4b71142444c0dfd7212d21d67e70e37
cg229836277 <1301958187@qq.com>
2016-04-10 22:34:55 +0800

    add self-design view to project

---
+++ FashionDiy/BottomChooseView.swift
@@ -0,0 +1,29 @@
+//
+//  BottomChooseView.swift
+//  FashionDiy
+//
+//  Created by chuck chan on 16/4/10.
+//  Copyright © 2016年 chuck chan. All rights reserved.
+//
+
+import UIKit
+
+class BottomChooseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
